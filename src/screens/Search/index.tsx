cd658c4c1ab637f2a2e9a984e2713531b56f3ee1
DanielFG07 <dan07f@gmail.com>
2023-05-30 12:27:51 -0300

    React Native, navegação entre telas simples com TypeScript

---
+++ src/screens/Search/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native/types";
+import { useNavigation} from "@react-navigation/native";
+
+import { propsStack } from "../../routes/Stack/Models";
+
+const Search = () => {
+    const navigation = useNavigation<propsStack>()
+    return (
+        <View style={{ flexDirection: "column", alignItems: 'center', justifyContent: "center", flex: 1}}>
+            <Text style={{ fontSize: 20}}>Home</Text>
+
+            <TouchableOpacity 
+            style={{ marginTop: 12,padding: 8, backgroundColor: "#BDBDBD"}}
+            onPress={() => navigation.navigate("Profile", {
+                name: "Daniel"
+            })}>
+                <Text>Profile</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+            style={{ marginTop: 12, padding: 8, backgroundColor: "#BDBDBD"}}
+            onPress={() => navigation.navigate("Search")}>
+                <Text>Search</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+            style={{ marginTop: 12, padding: 8, backgroundColor: "#BDBDBD"}}
+            onPress={() => navigation.goBack()}>
+                <Text>Voltar</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+
+export default Search
